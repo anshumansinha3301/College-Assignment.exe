d415755e1f799704ddc55edbe53ec9e798687eec
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2026-01-14 14:02:23 +0530

    Delete Ibm2.sql

--- Ibm2.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    name,
-    '2024-04-10' AS today_date,
-    DATE(next_renewal_date) AS next_renewal_date,
-    DATEDIFF(DATE(next_renewal_date), '2024-04-10') AS days_until_renewal
-FROM domains
-ORDER BY days_until_renewal ASC, name ASC;
